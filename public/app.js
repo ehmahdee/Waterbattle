8dd29ee4dd8875967ba1a9b13ec5c4507d55e83c
Fancy Pants <edaily94@gmail.com>
2023-05-12 14:54:26 -0700

    added consts to app.js

---
+++ public/app.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const userGrid = document.querySelector('grid-user')
+    const computerGrid = document.querySelector('.grid-computer')
+    const displayGrid = document.querySelector('.grid-display')
+    const ships = document.querySelectorAll('.ship')
+    const destroyer = document.querySelector('.destroyer-container')
+    const submarine = document.querySelector('.submarine-container')
+    const cruiser = document.querySelector('.cruiser-container')
+    const battleship = document.querySelector('.battleship-container')
+    const carrier = document.querySelector('.carrier-container')
+    const startButton = document.querySelector('#start')
+    const rotateButton = document.querySelector('#rotate')
+    const turnDisplay = document.querySelector('#whose-go')
+    const infoDisplay = document.querySelector('#info')
+    const setupButtons = document.getElementById('setup-buttons')
+    const userSquares = []
+    const computerSquares = []
+    let isHorizontal = true
+    let isGameOver = false
+    let currentPlayer = 'user'
+    const width = 10
+    let playerNum = 0
+    let ready = false
+    let enemyReady = false
+    let allShipsPlaced = false
+    let shotFired = -1
+})
