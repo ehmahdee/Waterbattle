50034598fae3559109bb8ad39e5017d1d3db31fa
Michrira <Michaelchristianrader@gmail.com>
2023-05-12 13:22:10 -0700

    updated connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,33 @@
 const mongoose = require('mongoose');
+const Sequelize = require('sequelize');
+require('dotenv').config();
 
+// Mongoose setup
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/techmatchup', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+const mongooseConnection = mongoose.connection;
 
-module.exports = mongoose.connection;
+// Sequelize setup
+let sequelize;
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306,
+    }
+  );
+}
+
+// Export connections
+module.exports = {
+  mongooseConnection,
+  sequelize,
+};
