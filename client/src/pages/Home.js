8aca6d670f7b8c29a76b0f7d30bceb8ebdbd9963
Fancy Pants <edaily94@gmail.com>
2023-05-15 12:38:56 -0700

    got the react up

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
+
+const Home = () => {
+    return (
+        <div class="button-container">
+        <h1>Water Battle!!</h1>
+        <button href="">Sign Up</button>
+        <button href="">Login</button>
+        </div>
+    )
+}
+
+export default Home;
