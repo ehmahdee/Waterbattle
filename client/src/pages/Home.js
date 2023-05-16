b62b1ad600cc2ddda57ceffe82dd118223c10e0a
Fancy Pants <edaily94@gmail.com>
2023-05-16 13:34:45 -0700

    committing to pull

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,7 +3,7 @@ import { useQuery } from '@apollo/client';
 
 const Home = () => {
     return (
-        <div class="button-container">
+        <div className="button-container">
         <h1>Water Battle!!</h1>
         <button href="">Sign Up</button>
         <button href="">Login</button>
