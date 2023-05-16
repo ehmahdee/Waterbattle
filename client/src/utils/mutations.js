827e00879247ba1d85458eb522e24f697eb919dd
seafoodude <michaelhuang777@gmail.com>
2023-05-16 13:22:55 -0700

    fixed up mutations and queries in util in src in client

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -24,33 +24,12 @@ export const ADD_USER = gql`
   }
 `;
 
-export const ADD_THOUGHT = gql`
-  mutation addThought($thoughtText: String!) {
-    addThought(thoughtText: $thoughtText) {
+export const ADD_MATCHDATA = gql`
+  mutation addMatch($result: String!) {
+    addMatch(result: $result) {
       _id
-      thoughtText
-      thoughtAuthor
+      result
       createdAt
-      comments {
-        _id
-        commentText
-      }
-    }
-  }
-`;
-
-export const ADD_COMMENT = gql`
-  mutation addComment($thoughtId: ID!, $commentText: String!) {
-    addComment(thoughtId: $thoughtId, commentText: $commentText) {
-      _id
-      thoughtText
-      thoughtAuthor
-      createdAt
-      comments {
-        _id
-        commentText
-        createdAt
-      }
     }
   }
 `;
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -15,35 +15,3 @@ export const QUERY_USER = gql`
   }
 `;
 
-export const QUERY_SINGLE_THOUGHT = gql`
-  query getSingleThought($thoughtId: ID!) {
-    thought(thoughtId: $thoughtId) {
-      _id
-      thoughtText
-      thoughtAuthor
-      createdAt
-      comments {
-        _id
-        commentText
-        commentAuthor
-        createdAt
-      }
-    }
-  }
-`;
-
-export const QUERY_ME = gql`
-  query me {
-    me {
-      _id
-      username
-      email
-      thoughts {
-        _id
-        thoughtText
-        thoughtAuthor
-        createdAt
-      }
-    }
-  }
-`;
