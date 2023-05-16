dd290ebf8d8168f7d101435fc873a1c41208ea49
seafoodude <michaelhuang777@gmail.com>
2023-05-16 11:47:53 -0700

    added singleplayer react page

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
+
   return (
     <div>
       <header>
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -15,17 +15,6 @@ export const QUERY_USER = gql`
   }
 `;
 
-export const QUERY_THOUGHTS = gql`
-  query getThoughts {
-    thoughts {
-      _id
-      thoughtText
-      thoughtAuthor
-      createdAt
-    }
-  }
-`;
-
 export const QUERY_SINGLE_THOUGHT = gql`
   query getSingleThought($thoughtId: ID!) {
     thought(thoughtId: $thoughtId) {
