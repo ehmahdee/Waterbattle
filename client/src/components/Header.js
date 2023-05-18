f1a3f3a16397de8ac2df85064328eb0b290db655
seafoodude <michaelhuang777@gmail.com>
2023-05-17 19:50:05 -0700

    formatted header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -6,15 +6,15 @@ const Header = () => {
   return (
     <div>
       <nav className="navbar">
-                <ul className="navbar-nav">
-                   {/*  <li className="nav-item"><a href="./highScores.html">High scores</a></li> */}
-                    <li className="nav-item"><a href="./lobby">New Game</a></li>
-                </ul>
-            </nav>
+        <ul className="navbar-nav">
+          {/*  <li className="nav-item"><a href="./highScores.html">High scores</a></li> */}
+          <li className="nav-item"><a href="./lobby">New Game</a></li>
+        </ul>
+      </nav>
       <header>
-        <a href="https://github.com/ehmahdee/Waterbattle" target="_blank"><img style={{width:'90px'}} src="../images/vaporeonlogo.png" alt="vaporeon" class="logo"/></a>
-    </header>
-    
+        <a href="https://github.com/ehmahdee/Waterbattle" target="_blank"><img style={{ width: '90px' }} src="../images/vaporeonlogo.png" alt="vaporeon" class="logo" /></a>
+      </header>
+
     </div>
   )
 }
