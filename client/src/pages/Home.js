99e7e3c4beee44604d4efd0a9b29b9828963bb48
Fancy Pants <edaily94@gmail.com>
2023-05-17 21:11:41 -0700

    set up for audio

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,11 +5,19 @@ import { Link } from 'react-router-dom';
 const Home = () => {
     return (
         <div>
-        <h1>Water Battle!!</h1>
-        <img style={{ width: '90px' }} src="./images/splashhero.png" alt="hero image" class="logo" />
-        <div className="button-container">
+        <h1 style={{ marginTop: '2rem' }}>Water Battle!!</h1>
+        <img style={{ width: '600px' }} src="../images/spashhero.png" alt="hero image" className="splash-logo" />
+        <div style={{ margin: '0rem', marginBottom: '2rem'}} className="button-container">
         <button><Link to="/signup">Sign Up</Link></button>
         <button><Link to="/login">Login</Link></button>
+        <audio src="/music/good_enough.mp3" controls>	
+	<embed 
+		src="/music/good_enough.mp3"
+		width="300"
+		height="90"
+		loop="true"
+		autostart="true"/>
+</audio>
         </div>
         </div>
     )
