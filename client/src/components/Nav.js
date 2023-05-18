3649fcd56257df5c962aee9f029ba3ac00db9eed
Patricia <124640646+P-Trish@users.noreply.github.com>
2023-05-18 13:33:21 -0700

    logout  works

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -3,16 +3,22 @@ import { Link } from 'react-router-dom';
 import Auth from '../utils/auth'
 
 const Nav = () => {
-const Logout = (event)  => {
-  event.preventDefault();
-  Auth.logout();
-}
+  const Logout = (event) => {
+    event.preventDefault();
+    Auth.logout();
+  }
   return (
     <div>
       <nav className="navbar">
         <ul className="navbar-nav">
-          <li className="nav-item"><a href="./lobby">New Game</a></li>
-          <li className="nav-item"><a onClick = {Logout}>Logout</a></li>
+          {Auth.loggedIn() ? (
+            <>
+              <li className="nav-item"><a href="./lobby">New Game</a></li>
+
+              <li className="nav-item"><a onClick={Logout}>Logout</a></li>
+            </>
+          ) : (null)}
+
         </ul>
 
       </nav>
